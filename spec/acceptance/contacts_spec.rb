ce596d0131e979a4fd49df5ce062bb3e7f2c1df2
Oladele <oladele@gmail.com>
2015-10-07 14:54:05 -0400

    initial acceptance test

---
+++ spec/acceptance/contacts_spec.rb
@@ -0,0 +1,14 @@
+RSpec.resource "Contacts" do
+  header "Content-Type", "application/vnd.api+json"
+
+  get "/contacts" do
+    before do
+      FactoryGirl.create :contact
+
+    end
+    example_request "List contacts" do
+      expect(status).to eq 200
+    end
+  end
+
+end
