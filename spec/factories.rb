06241a241415585864331b6608ace4a7dcb24db6
Oladele <oladele@gmail.com>
2015-10-07 13:40:34 -0400

    rails g model PhoneNumber contact_id:integer...
    rails g model PhoneNumber contact_id:integer name:string phone_number:string

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
-FactoryGirl.define do  factory :contact do
+FactoryGirl.define do  factory :phone_number do
+    contact_id 1
+name "MyString"
+phone_number "MyString"
+  end
+  factory :contact do
     name_first "MyString"
 name_last "MyString"
 email "MyString"
